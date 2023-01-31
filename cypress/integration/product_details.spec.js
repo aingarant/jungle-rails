5743451d5238fadd4b1f7f1cb68d885403b5ef92
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-31 10:11:20 -0500

    Added stretch cypress test user-login spec

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -13,7 +13,7 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+describe('Product Details', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,54 @@
+
+
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('User Login', () => {
+  // beforeEach(() => {
+  //   // Cypress starts out with a blank slate for each test
+  //   // so we must tell it to visit our website with the `cy.visit()` command.
+  //   // Since we want to visit the same URL at the start of all our tests,
+  //   // we include it in our beforeEach function so that it runs before each test
+  //   cy.visit('http://localhost:3000')
+
+  // })
+
+  it("Visit Home page", () =>{
+    cy.visit('http://localhost:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+
+  it("Show Login nav link", () => {
+    cy.get(".nav-link").should("contain.text", "Login");
+  });
+
+
+  it("Click on Login link and show login form", () => {
+    cy.get('.login-link').click({force:true})
+    cy.get('#email').type('aingarant@gmail.com')
+    cy.get('#password').type('aingaran')
+    cy.contains('Submit').click({force: true})
+    
+  });
+
+
+  it("Show user is logged in.", () => {
+
+    cy.get(".current_user").should("contain.text", "Welcome,");
+  });
+
+  })
